34cdcf6b657c59e62d470114ac4472fb33e05a8e
SelimFEUP <84468279+SelimFEUP@users.noreply.github.com>
2026-06-26 17:59:36 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -12,18 +12,53 @@ np.random.seed(seed)
 tf.random.set_seed(seed)
 
 class MaskedMSE(tf.keras.losses.Loss):
+    def __init__(self, F, lambda_obs=0.01, alpha=0.2):
+        super().__init__()
+        self.F = F
+        self.lambda_obs = lambda_obs
+        self.alpha = alpha
+
+    def compute_delta_t(self, mask):
+        mask = tf.cast(mask, tf.float32)
+
+        def step(prev, m):
+            return (1.0 - m) * (prev + 1.0)
+
+        delta_t = tf.scan(
+            fn=step,
+            elems=tf.transpose(mask, [1, 0, 2]),
+            initializer=tf.zeros_like(mask[:, 0, :])
+        )
+
+        delta_t = tf.transpose(delta_t, [1, 0, 2])
+        return tf.minimum(delta_t, 12.0)
+
     def call(self, y_true, y_pred):
-        values = y_true[..., :F]
-        mask   = y_true[..., F:]
+        values = y_true[..., :self.F]
+        mask   = y_true[..., self.F:]   # observed mask
 
-        mse_all = tf.reduce_mean(tf.square(y_pred - values))
-        mse_miss = tf.reduce_sum(tf.square((y_pred - values) * (1-mask))) / (tf.reduce_sum(1-mask)+1e-8)
+        missing_mask = 1.0 - mask
 
-        return 0.1 * mse_all + mse_miss
+        delta_t = self.compute_delta_t(mask)
+
+        # safer weighting
+        weights = 1.0 + self.alpha * delta_t
+
+        error = tf.square(y_pred - values)
+
+        missing_loss = tf.reduce_sum(error * missing_mask * weights) / (
+            tf.reduce_sum(missing_mask * weights) + 1e-8
+        )
+
+        observed_loss = tf.reduce_sum(error * mask) / (
+            tf.reduce_sum(mask) + 1e-8
+        )
+
+        return missing_loss + self.lambda_obs * observed_loss
 
 def train_model():
   model = build_imputation_model(X_train.shape[1:], F)
-  model.compile(optimizer='adam', loss=MaskedMSE())
+  model.compile(optimizer='adam', loss=MaskedMSE(F))
   model.summary()
   early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=15, restore_best_weights=True)
   mc = tf.keras.callbacks.ModelCheckpoint('models/pems_random.keras', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
