7133b37a4577bfb47a6cb6e0322a4a707f394f0a
SelimFEUP <84468279+SelimFEUP@users.noreply.github.com>
2026-08-10 15:52:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,14 @@ def main():
   
     # ---------- Inference ----------
     preds = model.predict(X_test, batch_size=32)
-    imputed_scaled = reconstruct_from_windows(preds, len(test_raw), window)
+    imputed_scaled = reconstruct_from_windows_weighted(preds=preds, T=len(test_scaled), window=window, F=F, stride=1, window_type="hanning")
     imputed_scaled[test_mask == 1] = test_scaled[test_mask == 1]
     imputed = scaler.inverse_transform(imputed_scaled)
     
     # Evaluate the model on the test set
-    rmse, mae = compute_errors(imputed, test_raw, test_eval_mask.values)
-    mape = compute_smape(imputed, test_raw, test_eval_mask.values)
+    rmse, mae = compute_errors(imputed, test_raw, test_eval_mask)
+    mape = compute_smape(imputed, test_raw, test_eval_mask)
+    
     print(f"\nFINAL → RMSE: {rmse:.4f}   MAE: {mae:.4f}  MAPE: {mape:.4f}")
     
 if __name__ == "__main__":
