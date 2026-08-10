5e71739613155b012060db17c0a1118290d6ae9c
SelimFEUP <84468279+SelimFEUP@users.noreply.github.com>
2026-08-10 15:47:14 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -3,13 +3,15 @@ from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import LSTM, Dense, Conv1D, BatchNormalization, ReLU, Add, Input, Lambda, MultiHeadAttention, Concatenate, LayerNormalization
 from tensorflow.keras import backend as K
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
 from src.model import build_imputation_model
 from src.preprocessing import *
 
 seed = 42
 np.random.seed(seed)
 tf.random.set_seed(seed)
+
+physical_adj = load_adj_matrix("bay_adj_mx.pkl")
 
 class MaskedMAE(tf.keras.losses.Loss):
     def __init__(self, F, lambda_obs=0.3):
@@ -34,10 +36,14 @@ class MaskedMAE(tf.keras.losses.Loss):
         return missing_loss + self.lambda_obs * observed_loss
 
 def train_model():
-  model = build_imputation_model(X_train.shape[1:], F)
-  model.compile(optimizer='adam', loss=MaskedMSE(F))
+  model = build_imputation_model(X_train.shape[1:], F, predefined_adj=physical_adj, dropout_rate=0.15, l2_reg=1e-4)
+  optimizer = tf.keras.optimizers.experimental.AdamW(learning_rate=1e-3, weight_decay=1e-5)
+  model.compile(optimizer=optimizer, loss=MaskedMAE(F, lambda_obs=0.8))
   model.summary()
-  early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=15, restore_best_weights=True)
-  mc = tf.keras.callbacks.ModelCheckpoint('models/pems_random.keras', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-  history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=32, validation_split=0.1, callbacks=[mc, early_stopping])
+  callbacks = [
+    ModelCheckpoint('pems_random.keras', monitor='val_loss', save_best_only=True, mode='min', verbose=1),
+    EarlyStopping(monitor='val_loss', patience=12, restore_best_weights=True),
+    ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=6, min_lr=1e-6)
+   ]
+  history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=150, batch_size=48, callbacks=callbacks)
   return history
